bbdedaa45429ad75ed7c74cde684486bef2f7775
Mariusz Ferdyn <MariuszFerdyn@users.noreply.github.com>
2024-08-31 16:28:01 +0000

    use_pulseio=True

--- 01-DHT11/InteractWithDHT11.py
+++ 01-DHT11/InteractWithDHT11.py
@@ -2,15 +2,22 @@ import time
 import adafruit_dht
 import board
 
-dht_device = adafruit_dht.DHT11(board.D17)
+dht_device = adafruit_dht.DHT11(board.D17, use_pulseio=True)
+max_retries = 20
+retry_delay = 5  # seconds
 
-try:
-    temperature = dht_device.temperature
-    temperature_f = temperature * (9 / 5) + 32 # Convert to Fahrenheit
+for attempt in range(max_retries):
+    try:
+        temperature = dht_device.temperature
+        #temperature_f = temperature * (9 / 5) + 32  # Convert to Fahrenheit
 
-    humidity = dht_device.humidity
+        humidity = dht_device.humidity
 
-    print(f'{{"Temperature": {temperature}, "Humidity": {humidity}}}')
-    #print("Temp:{:.1f} C / {:.1f} F    Humidity: {}%".format(temperature_c, temperature_f, humidity))
-except RuntimeError as err:
-    print(err.args[0])
+        print(f'{{"Temperature": {temperature}, "Humidity": {humidity}}}')
+        # print("Temp:{:.1f} C / {:.1f} F    Humidity: {}%".format(temperature, temperature_f, humidity))
+        break  # Exit the loop if successful
+    except RuntimeError as err:
+        print(f"Attempt {attempt + 1} failed: {err.args[0]}")
+        time.sleep(retry_delay)
+else:
+    print("Failed to read from DHT11 sensor after several attempts.")
