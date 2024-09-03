ef0066a7864a97d2659d27f7086c530a66382d3a
Mariusz Ferdyn <MariuszFerdyn@users.noreply.github.com>
2024-09-03 11:47:03 +0000

    added comment

--- 02-InfraRedReceiver/01-ReceiveInfraRedCodes.sh
+++ 02-InfraRedReceiver/01-ReceiveInfraRedCodes.sh
@@ -14,31 +14,11 @@ EOF
 # Overwrite /etc/lirc/hardware.conf
 sudo tee /etc/lirc/hardware.conf > /dev/null <<EOF
 
-########################################################
-# /etc/lirc/hardware.conf
-#
-# Arguments which will be used when launching lircd
-LIRCD_ARGS="--uinput"
-
-# Don't start lircmd even if there seems to be a good config file
-# START_LIRCMD=false
-
-# Don't start irexec, even if a good config file seems to exist.
-# START_IREXEC=false
-
-# Try to load appropriate kernel modules
+LIRCD_ARGS="--uinput --listen"
 LOAD_MODULES=true
-
-# Run "lircd --driver=help" for a list of supported drivers.
 DRIVER="default"
-# usually /dev/lirc0 is the correct setting for systems using udev
 DEVICE="/dev/lirc0"
 MODULES="lirc_rpi"
-
-# Default configuration files for your hardware if any
-LIRCD_CONF=""
-LIRCMD_CONF=""
-######################################################## 
 EOF
 
 # Append to /boot/config.txt
@@ -47,9 +27,17 @@ sudo tee -a /boot/config.txt > /dev/null <<EOF
 dtoverlay=lirc-rpi,gpio_in_pin=27,gpio_out_pin=22
 EOF
 
+# Not sure about this section
+sudo tee -a /etc/lirc/lirc_options.conf > /dev/null <<EOF
 
-sudo /etc/init.d/lirc stop
-sudo /etc/init.d/lirc start
+driver    = default
+device    = /dev/lirc0
+EOF
 
-sudo /etc/init.d/lirc stop
+
+sudo /etc/init.d/lircd stop
+sudo /etc/init.d/lircd start
+sudo /etc/init.d/lircd status
+
+sudo /etc/init.d/lircd stop
 mode2 -d /dev/lirc0
