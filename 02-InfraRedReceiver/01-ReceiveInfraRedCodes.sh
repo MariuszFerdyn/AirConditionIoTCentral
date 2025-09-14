be0632675265503888976760a1bd530c8526a1bf
Mariusz Ferdyn <MariuszFerdyn@users.noreply.github.com>
2025-09-14 17:30:00 +0200

    Refactor ReceiveInfraRedCodes.sh for clarity
    Removed unnecessary comments and cleaned up the script.

--- 02-InfraRedReceiver/01-ReceiveInfraRedCodes.sh
+++ 02-InfraRedReceiver/01-ReceiveInfraRedCodes.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 
-#https://gist.github.com/prasanthj/c15a5298eb682bde34961c322c95378b
 
 sudo apt update
 sudo apt-get install -y lirc
-
-# Append to /etc/modules (not needed anymore)
-# sudo tee -a /etc/modules > /dev/null <<EOF
-#lirc_dev
-#lirc_rpi gpio_in_pin=27 gpio_out_pin=22
-#EOF
 
 # Overwrite /etc/lirc/hardware.conf
 sudo tee /etc/lirc/hardware.conf > /dev/null <<EOF
@@ -27,7 +20,7 @@ dtoverlay=pwm-ir-tx,gpio_pin=18
 dtoverlay=gpio-ir,gpio_pin=22
 EOF
 
-# Not sure about this section
+# Set default device
 sudo tee -a /etc/lirc/lirc_options.conf > /dev/null <<EOF
 
 driver    = default
@@ -39,24 +32,5 @@ EOF
 sudo /etc/init.d/lircd stop
 sudo /etc/init.d/lircd start
 sudo /etc/init.d/lircd status
-
 sudo /etc/init.d/lircd stop
-#mode2 -d /dev/lirc0
-#mode2 -H default -d /dev/lirc0
-
 sudo reboot
-
-ir-ctl -f -d /dev/lirc0      # Display information about lirc0 interface - observer which one is for receiving, which one is for sending
-ir-ctl -f -d /dev/lirc1      # Display information about lirc1 interface - observer which one is for receiving, which one is for sending
-
-ir-ctl -r       # And press on Remote control button to learn the codes to send - you shoud receive something like:
-                # mf@pi1:~/AirConditionIoTCentral/02-InfraRedReceiver $ ir-ctl -r
-                # +8984 -4465 +612 -1634 +617 -1634 +617 -506 +619 -502 +616 -507 +617 -506 +616 -1645 +608
-
-
-ir-ctl -ron.ir    # And press on Remote control button to learn the codes to send - repeat every button you want to program. - commands will be stored in on.ir file
-ir-ctl -roff.ir   # And press on Remote control button to learn the codes to send - repeat every button you want to program. - commands will be stored in off.ir file In that case I record on and off.
-
-#Now lets test if we can send codes
-ir-ctl -son.ir    # We are sending on message via IR - so IR receiver in that case should be on
-ir-ctl -soff.ir   #We are sending on message via IR - so IR receiver in that case should be off
